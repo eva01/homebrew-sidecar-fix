fd4aeaf919307c79f6598eddd30f0edd5d44d719
Shee Tze Jin <4400814+eva01@users.noreply.github.com>
2026-06-26 09:42:57 +0800

    Bump formula to v0.6.2

--- Formula/sidecar-fix.rb
+++ Formula/sidecar-fix.rb
@@ -1,38 +1,15 @@
 class SidecarFix < Formula
-  desc "Auto-restore Sidecar display arrangement via CoreGraphics display callbacks"
+  desc "Auto-restore Sidecar display arrangement with a lightweight launchd daemon"
   homepage "https://github.com/eva01/sidecar-fix"
-  url "https://github.com/eva01/sidecar-fix/releases/download/v0.6.1/sidecar-fix-v0.6.1-macos.tar.gz"
-  sha256 "abb1fddc3d74cd57f2cf3e83b06e73169911b1c244d6d96d017385ebdae3444c"
-  version "0.6.1"
+  url "https://github.com/eva01/sidecar-fix/releases/download/v0.6.2/sidecar-fix-v0.6.2-macos.tar.gz"
+  sha256 "0062689c47ef87da92db58b1301dec069ab46a499b22bb9a6ff2f22772ea52b5"
+  version "0.6.2"
   license "MIT"
 
   depends_on :macos
 
   def install
     bin.install "sidecar-fix"
-    (prefix/"com.jin.sidecar-fix.plist").write plist_content
-  end
-
-  def plist_content
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>com.jin.sidecar-fix</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/sidecar-fix</string>
-          <string>daemon</string>
-        </array>
-        <key>KeepAlive</key>
-        <true/>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
   end
 
   def caveats
